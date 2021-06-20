bb22cd3f405b248580dd9c7bb5835c157b93c7d4
YuanXiaode <yuanxiaode12@163.com>
2021-06-20 17:56:24 +0800

    com620

--- utils/datasets.py
+++ utils/datasets.py
@@ -256,7 +256,7 @@ class LoadStreams:  # multiple IP or RTSP cameras
         return 0  # 1E12 frames = 32 streams at 30 FPS for 30 years
 
 ## 输出： img (bs,h,w,c)  RGB格式 0-255
-##       label (N,6)  nl 指label的数量，后面的6指的是 (image_index,class,x,y,w,h)  x,y,w,h 是归一化的,N指一个bs中所有的label
+##       label (N,6)  N指一个bs中所有label的数量，后面的6指的是 (image_index,class,x,y,w,h)  x,y,w,h 是归一化的
 ##       path  元组， 每个元素都是图片路径
 ##       shapes 元组，每个元素都是 (h0, w0), ((h / h0, w / w0), pad)
 class LoadImagesAndLabels(Dataset):  # for training/testing
